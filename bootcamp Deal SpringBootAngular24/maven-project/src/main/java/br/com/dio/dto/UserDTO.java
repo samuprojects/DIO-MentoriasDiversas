248ee2cbbae01d478281123ad6992715fdafdf8d
Samuel <samugit.projects@gmail.com>
2024-11-04 22:30:38 -0300

    Módulo Trabalhando com Maven

---
+++ bootcamp Deal SpringBootAngular24/maven-project/src/main/java/br/com/dio/dto/UserDTO.java
@@ -0,0 +1,17 @@
+package br.com.dio.dto;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class UserDTO {
+
+    private int id;
+
+    private String name;
+    
+    private LocalDate birthday;
+
+
+}
